42c10369f39140247796d59d934311a1daba73ad
Codeur Libre <librecodeur@gmail.com>
2023-02-19 15:01:08 +0000

    Projet terminé du TP scroll & popup

--- script.js
+++ script.js
@@ -7,3 +7,34 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+//? -------------------------------------
+
+//! Scrool event
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  var currentScroll = window.scrollY;
+
+  if (currentScroll > lastScroll) {
+    navbar.style.height = "60px";
+  } else {
+    navbar.style.height = "90px";
+  }
+  lastScroll = currentScroll;
+
+  if (currentScroll >= 450) {
+    imgImprovise.style.opacity = "1";
+    imgImprovise.style.transform = "translateX(0)";
+  }
+  if (currentScroll >= 1220) {
+    popup.classList.add("affichePopup");
+  }
+});
+
+//! Click event
+closeBtn.addEventListener("click", () => {
+  console.log("Close");
+  popup.style.transform = "translateX(600px)";
+  popup.style.transition = "0.6s ease-out";
+  //   transform: translateX(400px);
+});
